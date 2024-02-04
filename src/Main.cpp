dff40b42ef9f48ce9521b06fbd6cdf6c985e6abf
laurenzooz <lauri.pahkala@hotmail.com>
2024-02-04 18:41:01 +0200

    Create window

---
+++ src/Main.cpp
@@ -0,0 +1,57 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <array>
+
+const unsigned int width = 640;
+const unsigned int height = 480;
+
+int main()
+{
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+	// Create window and initialize glfw
+	GLFWwindow* window;
+	if (!glfwInit()) 
+	{
+		std::cout << "Failed GLFW initialization" << std::endl;
+		return -1;
+	}
+
+	window = glfwCreateWindow(width, height, "OpenGL-cube", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		std::cout << "Failed creating window" << std::endl;
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	// initialize glew
+	if (glewInit() != GLEW_OK) 
+	{
+		std::cout << "Failed Glew initialization" << std::endl;
+		return -1;
+	}
+
+	// Main loop
+	while (!glfwWindowShouldClose(window))
+	{
+
+	}
+
+	
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
+}
