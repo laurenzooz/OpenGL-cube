b655ff4b78ca43119e677c95a9eea2cb6d4311ee
laurenzooz <lauri.pahkala@hotmail.com>
2024-02-05 13:06:26 +0200

    remove debug code

--- src/Buffer.cpp
+++ src/Buffer.cpp
@@ -23,16 +23,12 @@ void Buffer::setData(std::vector <float> &data, GLuint usage)
 void Buffer::setData(std::vector <unsigned int> &data, GLuint usage)
 {
 	//this->bind(); // the buffer needs to be bind to set the data
-	std::cout << data[0] << data[1] << data[2];
-	std::cout << "data size: " << data.size() << " " << data.size() * sizeof(unsigned int);
-	std::cout << type;
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, data.size() * sizeof(unsigned int), data.data(), usage);
 	//this->unbind();
 }
 
 void Buffer::bind()
 {
-	if (type == GL_ELEMENT_ARRAY_BUFFER) std::cout << "stemmar";
 	glBindBuffer(type, id);
 }
 
